38e1d5ddb0bba0d8d571b4f33894a9e6872e3bb0
icarus2176 <ashe.kemuri@gmail.com>
2023-07-07 16:19:45 -0500

    create models with associations

---
+++ db/migrate/20230704214630_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.belongs_to :posts
+      t.belongs_to :user
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
