9a23729253ad3ef0d370036dfcd0e3ebea0430d4
icarus2176 <ashe.kemuri@gmail.com>
2023-07-07 17:09:36 -0500

    fix validations and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: true, length: { in: 3...30 }
+  validates :body, presence: true, length: { minimum: 10  }
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
+
+  validates :username, presence: true, length: { in: 3...20 }
+  validates :password, presence: true, length: { in: 8...25 }
 end
